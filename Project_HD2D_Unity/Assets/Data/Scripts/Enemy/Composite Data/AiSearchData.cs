365476f715d6127dcbc663f95a5a8852839542f0
Victor Baz <>
2026-03-25 09:44:22 -0400

    change folder an other

--- Project_HD2D_Unity/Assets/Data/Scripts/Enemy/Composite Data/AiSearchData.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "AiSearchData", menuName = "Enemy/AiSearchData")]
-public class AiSearchData : ScriptableObject
-{
-    [field:SerializeField] public float searchDuration { get; private set; } = 10f;
-    [field:SerializeField] public float searchRadius { get; private set; } = 5f;
-}

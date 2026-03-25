82466346ddf50eaa901a029ce831d6c5f46f200d
Victor Baz <>
2026-03-25 09:44:22 -0400

    change folder an other

--- Project_HD2D_Unity/Assets/Data/Scripts/Enemy/Composite Data/AiTakeDamageData.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "AiTakeDamageData", menuName = "Enemy/AiTakeDamageData")]
-public class AiTakeDamageData : ScriptableObject
-{
-   [field:SerializeField] public int DamageToApply { get; private set; } = 1;
-   [field:SerializeField] public float StunDuration { get; private set; } = 0.2f;
-}

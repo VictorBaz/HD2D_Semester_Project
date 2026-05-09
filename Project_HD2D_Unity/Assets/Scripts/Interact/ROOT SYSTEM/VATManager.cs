78dcb1c402555070318093e8260509a066b89e15
Victor Baz <>
2026-05-09 15:48:47 -0400

    scan number by energy level

--- Project_HD2D_Unity/Assets/Scripts/Interact/ROOT SYSTEM/VATManager.cs
+++ Project_HD2D_Unity/Assets/Scripts/Interact/ROOT SYSTEM/VATManager.cs
@@ -99,6 +99,19 @@ public class VATManager : MonoBehaviour, IRootLink
             }
             else
             {
+                psScan.SetSubEmittersProbability(0);
+                switch (CurrentEnergy)
+                {
+                    case 2:
+                        psScan.SetSubEmitterProbability(0,1);
+                        break;
+                    case 3: 
+                        psScan.SetSubEmitterProbability(0,1);
+                        psScan.SetSubEmitterProbability(1,1);
+                        break;
+                    default:
+                        break;
+                }
                 psScan.TriggerParticleSystem();
             }
         }
